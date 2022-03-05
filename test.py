dffa9d350e1da6eef76dbeb738fc2af56f50a931
Ashuin Sharma <ashuin.sharma@gmail.com>
2022-03-05 10:55:42 -0600

    add mkdown

--- test.py
+++ test.py
@@ -2,5 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+st.title("Test app")
+
+st.markdown("Test")
+
+st.header("Tabla")
 dataframe = np.random.randn(10, 20)
 st.dataframe(dataframe)
